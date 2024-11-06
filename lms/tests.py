36d38ab58432eebf8dd9009e98f15c915f673ced
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-11-06 20:09:05 +0500

    Course 25.2(3)

--- lms/tests.py
+++ lms/tests.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
 from users.models import User
 
 
@@ -56,3 +56,24 @@ class LmsTestCase(APITestCase):
         self.client.force_authenticate(user=self.user)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class SubscriptionTestCase(APITestCase):
+    """Тест функционала работы подписки на обновления курса"""
+
+    def setUp(self):
+        self.user = User.objects.create(email="admin@example.com")
+        self.course = Course.objects.create(title="Test Course", description="Test Course description")
+        self.subscription = Subscription.objects.create(user=self.user, course=self.course)
+        self.client.force_authenticate(user=self.user)
+
+    def test_subscription_create(self):
+        url = reverse("lms:course_subscribe")
+        data = {
+            "user": self.user.pk,
+            "course": self.course.pk,
+        }
+        response = self.client.post(url, data)
+        response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), {"message": "Подписка отключена"})
